996f9e76c1725a7594091c03f8e31cb3fe8d651f
JUSTME <674987879@qq.com>
2019-12-24 20:00:41 +0800

    图片居中，删除图片标题

--- themes/hexo-theme-icarus/source/js/main.js
+++ themes/hexo-theme-icarus/source/js/main.js
@@ -2,10 +2,11 @@
     $('.article img:not(".not-gallery-item")').each(function () {
         // wrap images with link and add caption if possible
         if ($(this).parent('a').length === 0) {
-            $(this).wrap('<a class="gallery-item" href="' + $(this).attr('src') + '"></a>');
-            if (this.alt) {
-                $(this).after('<div class="has-text-centered is-size-6 has-text-grey caption">' + this.alt + '</div>');
-            }
+        	//$(this).wrap('<a class="gallery-item" href="' + $(this).attr('src') + '"></a>'); 图片靠左
+            $(this).wrap('<a class="gallery-item" style="display:block;text-align:center;" href="' + $(this).attr('src') + '"></a>');
+//            if (this.alt) {
+//                $(this).after('<div class="has-text-centered is-size-6 has-text-grey caption">' + this.alt + '</div>');
+//            }
         }
     });
 
